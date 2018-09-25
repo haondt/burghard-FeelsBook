4e95c77d62dcff32bba1b6dce264d686767e72da
haondt <burghardtnoah@gmail.com>
2018-09-25 16:33:05 -0600

    added shadows to buttons

--- app/src/main/java/com/example/noahburghardt/burghard_feelsbook/MainActivity.java
+++ app/src/main/java/com/example/noahburghardt/burghard_feelsbook/MainActivity.java
@@ -21,12 +21,28 @@ package com.example.noahburghardt.burghard_feelsbook;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView moodHistory;
+    private FeelingList feelings;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.feelings = new FeelingList();
+        
+
+    }
+
+    public void addAngry(View view){
+
+        Toast.makeText(this, "Added angry emotion", Toast.LENGTH_SHORT).show();
     }
 }
